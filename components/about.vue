410572b66281122a7592a26ecd502fa7e8a464b5
Allan de Lima <allandelima@MacBook-Pro-de-Allan.local>
2019-07-25 13:47:30 -0300

    Adição de imagem no sobre

--- components/about.vue
+++ components/about.vue
@@ -1,9 +1,10 @@
 <template>
-  <section class="section" id="about">
-    <div class="container is-flex" >
+  <section class="section is-relative" id="about">
+    <div class="container is-flex is-clipped" >
+      <div class="background"></div>
       <div class="has-text-centered content">
-        <h1 class="title">O Nerd Experience é sobre</h1>
-        <p class="is-size-4">trazer aos jovens de diversas regiões do nosso estado um ambiente de intensa imersão que crie oportunidades de viver verdadeiramente o universo repleto de tecnologia, conteúdo nerd, gamer e geek</p >
+        <h1 class="title has-text-white">O Nerd Experience é sobre</h1>
+        <p class="is-size-4 has-text-white">trazer aos jovens de diversas regiões do nosso estado um ambiente de intensa imersão que crie oportunidades de viver verdadeiramente o universo repleto de tecnologia, conteúdo nerd, gamer e geek</p >
       </div>
     </div>
   </section>
@@ -18,18 +19,32 @@ export default Vue.extend({
 
 .section {
   @media (max-width: 768px) {
-    background: rgba(240,240,240,1);
+    background: url('~assets/images/1-min.jpg');
+    background-size: cover;
+    background-position: 50px -100px;
   }
 }
 
+.background {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  z-index: 1;
+  background: rgba(0,0,0,.6);
+}
+
 .section .container {
-  background: rgba(240,240,240,1);
+  background: url('~assets/images/1-min.jpg');
+  background-size: cover;
+  background-position: 50px -100px;
   padding: 50px 0px;
   border-radius: 5px;
 }
 
 .content {
   width: 50%;
+  z-index: 2;
   @media (max-width: 768px) {
     width: 100%;
   }
